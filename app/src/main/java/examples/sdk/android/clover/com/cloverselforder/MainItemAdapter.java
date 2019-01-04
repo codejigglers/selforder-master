f21f04f7c49621b3bf3684cfa3795e4baf739dcf
saumya <saumyasinha023@gmail.com>
2019-01-04 23:57:00 +0530

    fifth commit

---
+++ app/src/main/java/examples/sdk/android/clover/com/cloverselforder/MainItemAdapter.java
@@ -0,0 +1,63 @@
+package examples.sdk.android.clover.com.cloverselforder;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import examples.sdk.android.clover.com.cloverselforder.pojo.MainItems;
+
+public class MainItemAdapter extends RecyclerView.Adapter<MainItemAdapter.MyViewHolder> {
+    private Context mContext;
+    private MainItems[] mainItems;
+
+    public MainItemAdapter(Context mContext, MainItems[] mainItems) {
+        this.mContext = mContext;
+        this.mainItems = mainItems;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.card_view, viewGroup, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder viewHolder, int i) {
+        ArrayList<MainItems> mainItem = new ArrayList<>(Arrays.asList(mainItems));
+        Log.e("sam", mainItem.get(i).toString());
+        viewHolder.item.setText(mainItem.get(i).getItem());
+        viewHolder.price.setText(mainItem.get(i).getPrice());
+        viewHolder.description.setText(mainItem.get(i).getDescription());
+        // loading album cover using Glide library
+        ;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mainItems.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView item;
+        TextView price;
+        TextView description;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            item = (TextView) itemView.findViewById(R.id.item);
+            price = (TextView) itemView.findViewById(R.id.price);
+            description = (TextView) itemView.findViewById(R.id.description);
+        }
+    }
+}
--- app/src/main/java/examples/sdk/android/clover/com/cloverselforder/PlaceholderFragment.java
+++ app/src/main/java/examples/sdk/android/clover/com/cloverselforder/PlaceholderFragment.java
@@ -2,70 +2,74 @@ package examples.sdk.android.clover.com.cloverselforder;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import examples.sdk.android.clover.com.cloverselforder.R;
-import examples.sdk.android.clover.com.cloverselforder.pojo.Items;
-import examples.sdk.android.clover.com.cloverselforder.pojo.MainItems;
 
 import java.lang.reflect.Array;
 
+import examples.sdk.android.clover.com.cloverselforder.pojo.Items;
+import examples.sdk.android.clover.com.cloverselforder.pojo.MainItems;
+
 public class PlaceholderFragment extends Fragment {
-  /**
-   * The fragment argument representing the section number for this
-   * fragment.
-   */
-  private static final String ARG_SECTION_NUMBER = "section_number";
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    static Items[] items;
+    int tabNum;
 
-  int tabNum;
-  static Items[] items;
-
-  public PlaceholderFragment() {
-  }
-
-  public void setTab(int n) {
-    tabNum = n;
-  }
-
-  /**
-   * Returns a new instance of this fragment for the given section
-   * number.
-   */
-  public static PlaceholderFragment newInstance(int sectionNumber, Items[] item) {
-    try {
-      items = item.clone();
-    }
-    catch (Exception e) {
-
-    }
-    PlaceholderFragment fragment = new PlaceholderFragment();
-    Bundle args = new Bundle();
-    args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-    fragment.setArguments(args);
-    return fragment;
-  }
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-
-
-
-    View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
-    TextView textView = rootView.findViewById(R.id.section_label);
-    try {
-      Items item = (Items) Array.get(items, tabNum - 1);
-      MainItems[] mainItems = item.getMainItems();
-      MainItems mainItems1 = (MainItems) Array.get(mainItems, 0);
-      textView.setText(mainItems1.getDescription());
-    }
-    catch (Exception e) {
-
+    public PlaceholderFragment() {
     }
 
-    //textView.setText();
-    return rootView;
-  }
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PlaceholderFragment newInstance(int sectionNumber, Items[] item) {
+        try {
+            items = item.clone();
+        } catch (Exception e) {
+
+        }
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void setTab(int n) {
+        tabNum = n;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
+
+//        TextView textView = rootView.findViewById(R.id.section_label);
+        try {
+            Items item = (Items) Array.get(items, tabNum - 1);
+            MainItems[] mainItems = item.getMainItems();
+            MainItemAdapter mainItemAdapter = new MainItemAdapter(getContext(), mainItems);
+            RecyclerView recyclerView = rootView.findViewById(R.id.recycler_view);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.setAdapter(mainItemAdapter);
+//      MainItems mainItems1 = (MainItems) Array.get(mainItems, 0);
+//      textView.setText(mainItems1.getDescription());
+        } catch (Exception e) {
+
+        }
+
+        //textView.setText();
+        return rootView;
+    }
 }
